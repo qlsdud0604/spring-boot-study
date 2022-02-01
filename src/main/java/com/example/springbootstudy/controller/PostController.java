81cfd0e8920631f2684ff1b8f6a775b4b7814993
임빈영 <qlsdud960604@gmail.com>
2022-02-01 14:20:37 +0900

    POST API 실습

---
+++ src/main/java/com/example/springbootstudy/controller/PostController.java
@@ -0,0 +1,32 @@
+package com.example.springbootstudy.controller;
+
+import com.example.springbootstudy.dto.MemberDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class PostController {
+
+    @PostMapping(value = "/default")
+    public String postMethod() {
+        return "Hello World";
+    }
+
+    @PostMapping("/member01")
+    public String postMember(@RequestBody Map<String, String> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PostMapping(value = "member02")
+    public String postMemberDto(@RequestBody MemberDTO memberDTO) {
+        return memberDTO.toString();
+    }
+}
