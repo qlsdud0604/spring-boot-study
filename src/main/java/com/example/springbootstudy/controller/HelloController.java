cc694f96d5d91f3db8ab2f7af44094e2d226e592
임빈영 <qlsdud960604@gmail.com>
2022-02-09 11:45:35 +0900

    Logback 적용

--- src/main/java/com/example/springbootstudy/controller/HelloController.java
+++ src/main/java/com/example/springbootstudy/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.springbootstudy.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("log-test")
+    public void logTest() {
+        LOGGER.trace("Trace Log");
+        LOGGER.debug("Debug Log");
+        LOGGER.info("Info Log");
+        LOGGER.warn("Warn Log");
+        LOGGER.error("Error Log");
     }
 }
 
--- src/main/java/com/example/springbootstudy/controller/ProductController.java
+++ src/main/java/com/example/springbootstudy/controller/ProductController.java
@@ -2,12 +2,16 @@ package com.example.springbootstudy.controller;
 
 import com.example.springbootstudy.data.dto.ProductDTO;
 import com.example.springbootstudy.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/product-api")
 public class ProductController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
 
     private ProductService productService;
 
@@ -18,7 +22,15 @@ public class ProductController {
 
     @GetMapping(value = "/product/{productId}")
     public ProductDTO getProduct(@PathVariable String productId) {
-        return productService.getProduct(productId);
+
+        long startTime = System.currentTimeMillis();
+        LOGGER.info("[Product Controller] perform {} of Spring Study API", "getProduct()");
+
+        ProductDTO productDTO = productService.getProduct(productId);
+
+        LOGGER.info("[Product Controller] Response :: productId = {}, productName = {}, productPrice = {}, productStock ={}, Response Time = {}ms", productDTO.getProductId(), productDTO.getProductName(), productDTO.getProductPrice(), productDTO.getProductPrice(), productDTO.getProductStock(), (System.currentTimeMillis() - startTime));
+
+        return productDTO;
     }
 
     @PostMapping(value = "/product")
