4c93b0938b07069190175f757d716bc428eb18ed
임빈영 <qlsdud960604@gmail.com>
2022-02-10 11:54:01 +0900

    Validation 적용

--- src/main/java/com/example/springbootstudy/controller/ProductController.java
+++ src/main/java/com/example/springbootstudy/controller/ProductController.java
@@ -5,7 +5,11 @@ import com.example.springbootstudy.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/product-api")
@@ -24,23 +28,39 @@ public class ProductController {
     public ProductDTO getProduct(@PathVariable String productId) {
 
         long startTime = System.currentTimeMillis();
-        LOGGER.info("[Product Controller] perform {} of Spring Study API", "getProduct()");
+        LOGGER.info("[getProduct] perform {} of Spring Study API", "getProduct()");
 
         ProductDTO productDTO = productService.getProduct(productId);
 
-        LOGGER.info("[Product Controller] Response :: productId = {}, productName = {}, productPrice = {}, productStock ={}, Response Time = {}ms", productDTO.getProductId(), productDTO.getProductName(), productDTO.getProductPrice(), productDTO.getProductPrice(), productDTO.getProductStock(), (System.currentTimeMillis() - startTime));
+        LOGGER.info("[Product Controller] Response :: productId = {}, productName = {}, productPrice = {}, " +
+                        "productStock ={}, Response Time = {}ms", productDTO.getProductId(), productDTO.getProductName(),
+                productDTO.getProductPrice(), productDTO.getProductStock(),
+                (System.currentTimeMillis() - startTime));
 
         return productDTO;
     }
 
     @PostMapping(value = "/product")
-    public ProductDTO createProduct(@RequestBody ProductDTO productDTO) {
+    public ResponseEntity<ProductDTO> createProduct(@Valid @RequestBody ProductDTO productDTO) {
+
+//        /** Validation Code Example */
+//        if (productDTO.getProductId().equals("") || productDTO.getProductId().isEmpty()) {
+//            LOGGER.error("[createProduct] Failed Response :: productId id Empty");
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(productDTO);
+//        }
+
         String productId = productDTO.getProductId();
         String productName = productDTO.getProductName();
         int productPrice = productDTO.getProductPrice();
         int productStock = productDTO.getProductStock();
 
-        return productService.saveProduct(productId, productName, productPrice, productStock);
+        ProductDTO response = productService.saveProduct(productId, productName, productPrice, productStock);
+
+        LOGGER.info("[createProduct] Response :: productId = {}, productName = {}, productPrice = {}, " +
+                        "productStock ={}, Response Time = {}ms", response.getProductId(), response.getProductName(),
+                response.getProductPrice(), response.getProductStock());
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 }
--- src/main/java/com/example/springbootstudy/data/dto/ProductDTO.java
+++ src/main/java/com/example/springbootstudy/data/dto/ProductDTO.java
@@ -2,6 +2,10 @@ package com.example.springbootstudy.data.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -9,11 +13,19 @@ import lombok.*;
 @Builder
 public class ProductDTO {
 
+    @NotNull
     private String productId;
 
+    @NotNull
     private String productName;
 
+    @NotNull
+    @Min(value = 500)
+    @Max(value = 3000000)
     private int productPrice;
 
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 9999)
     private int productStock;
 }
