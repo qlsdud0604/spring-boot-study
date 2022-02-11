9c2db67d310054f520259a6e21d85139c219737a
임빈영 <qlsdud960604@gmail.com>
2022-02-11 18:13:21 +0900

    @ControllerAdvice, @ExceptionHandler를 이용한 예외 처리

--- src/main/java/com/example/springbootstudy/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/springbootstudy/exception/GlobalExceptionHandler.java
@@ -1,38 +1,20 @@
-package com.example.springbootstudy.controller;
+package com.example.springbootstudy.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
-public class HelloController {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
-
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello() {
-        return "Hello World";
-    }
-
-    @PostMapping("log-test")
-    public void logTest() {
-        LOGGER.trace("Trace Log");
-        LOGGER.debug("Debug Log");
-        LOGGER.info("Info Log");
-        LOGGER.warn("Warn Log");
-        LOGGER.error("Error Log");
-    }
-
-    @PostMapping("/exception")
-    public void exceptionTest() throws Exception {
-        throw new Exception();
-    }
+    private final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<Map<String, String>> ExceptionHandler(Exception e) {
@@ -40,7 +22,7 @@ public class HelloController {
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
 
         LOGGER.info(e.getLocalizedMessage());
-        LOGGER.info("Controller 내 ExceptionHandler() 호출");
+        LOGGER.info("GlobalExceptionHandler 내 ExceptionHandler() 호출");
 
         Map<String, String> map = new HashMap<>();
         map.put("error type", httpStatus.getReasonPhrase());
@@ -50,7 +32,3 @@ public class HelloController {
         return new ResponseEntity<>(map, responseHeaders, httpStatus);
     }
 }
-
-
-
-
